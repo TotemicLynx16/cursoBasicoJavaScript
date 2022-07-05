262ae18041d111f74376e24c24951bad5d5b58d6
totemiclynx <marlon9945@gmail.com>
2022-07-05 09:18:55 -0500

    Valores, funciones y variables

---
+++ valores-funciones-sintaxis/valores.js
@@ -0,0 +1,12 @@
+40
+
+"Marlon Sandoval"
+
+true 
+false
+
+null
+undefined
+
+[1,2,3]
+{nombre: "Marlon"}
---
+++ valores-funciones-sintaxis/variables.js
@@ -0,0 +1,13 @@
+//Asignar variable
+var nombre = "Marlon";
+//Declarar
+var edad;
+//inicializar
+edad = 30;
+
+var elementos = ["computadora", "Celular"];
+
+var persona = {
+    nombre: "Marlon",
+    edad: 23
+}
